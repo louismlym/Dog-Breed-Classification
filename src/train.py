0ccfe278c9f1ef44e5cda4c3e40c2221092e4a87
Louis <maliyp@cs.washington.edu>
2021-06-07 05:56:44 -0700

    investigate epoch printing

--- src/train.py
+++ src/train.py
@@ -130,11 +130,11 @@ def train_epochs(net, data, epochs=1, start_epoch=0, lr=0.01, momentum=0.9, deca
                         print('Epoch: {} Batch {} [{}/{} ({:.2f}%)] loss: {:.4f} accuracy: {:.6f}'.format(
                             epoch,
                             i + 1,
-                            i * BATCH_SIZE,
+                            (i + 1) * BATCH_SIZE,
                             len(dataloader.dataset),
-                            100.0 * i / len(dataloader),
+                            100.0 * (i + 1) / len(dataloader),
                             sum_loss / print_every,
-                            sum_correct / (i * BATCH_SIZE)
+                            sum_correct / ((i + 1) * BATCH_SIZE)
                             ))
                     sum_loss = 0.0
         avg_loss = epoch_loss / len(dataloader.dataset)
